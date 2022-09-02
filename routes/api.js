f8b8e594c81cd3f372e8c6414127cdf2863c78bb
PLCoster <58193305+PLCoster@users.noreply.github.com>
2022-09-02 15:36:11 -0700

    Updated API routes to utilise passed in DB client, and set up middleware using the DB connection
    POST /api/issues/:project route implemented so far

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,35 @@
 'use strict';
+const issueControllerSetup = require('../controllers/issueController');
 
-module.exports = function (app) {
+const DB_NAME = process.env.DB_NAME;
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+module.exports = async function (app, dbClient) {
+  const issuesCollection = await dbClient.db(DB_NAME).collection('issues');
+
+  // Create TTL index to expire documents after X seconds
+  // https://www.mongodb.com/docs/manual/tutorial/expire-data/
+  issuesCollection.createIndex({ createdAt: 1 }, { expireAfterSeconds: 10 });
+
+  // Set up issueController middleware with database connection
+  const { createNewIssue } = issueControllerSetup(issuesCollection);
+
+  app
+    .route('/api/issues/:project')
+
+    .get(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
+
+    // POST route to handle creating a new issue for a project
+    .post(createNewIssue, function (req, res) {
+      return res.json(res.locals.issueDoc);
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       let project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       let project = req.params.project;
-      
     });
-    
 };
